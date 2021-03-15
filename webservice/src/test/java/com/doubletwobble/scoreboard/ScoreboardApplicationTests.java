8f8df772aae3a4aa77b82654cbd4969a5945e24f
Delfs <martind_64@hotmail.com>
2021-03-15 01:07:26 +0100

    initial commit

---
+++ webservice/src/test/java/com/doubletwobble/scoreboard/ScoreboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doubletwobble.scoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoreboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
